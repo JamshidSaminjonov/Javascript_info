a8f4c0ad13ed1933cef6fdd422b39c999018ecf4
Jamshid <jsaminjonov5705@gmail.com>
2023-09-12 07:34:23 +0500

    Number, math

---
+++ 8-dars/main.js
@@ -0,0 +1,102 @@
+//   let n= 45.123;
+//   console.log(n)
+// console.log(parseFloat(n))
+// console.log(parseInt(n))
+// console.log(Number(n))
+// let a = parseInt(n)
+
+// console.log(1e12)//buning chegarasi bor 500ga borganda  infinity chiqadi
+// console.log(1e+61)
+// console.log(1e+500)
+// console.log(0.1+0.2);//oxirini yaxlitlaydi
+// console.log(0.2+0.4==0.6);
+// console.log(0.1+0.2);//0.3000000004
+// console.log(+Number(.1+.2).toFixed(1));
+
+// console.log(parseFloat(num))//biinchi sonni hammasini oladi
+// console.log(parseInt(num))// birinchi sonnni butunini oladi
+// console.log(Number(num))// stringda amal bajaromiydi
+// console.log(eval(num))//// shu ishlaydi eval
+// console.log(num.toString(5))
+// console.log(parseInt(101,5))
+// console.log(Object.is('web','web1'))// taqqoslash
+// console.log(num.toPrecision(2))
+// console.log(num1.toPrecision(2))// nollarni ignore qivoradi
+
+///strage
+
+// console.log(9_999_999_999_999_999)// shu sondan boshlab 1 qoshvoradi  space yetmaydi diydi
+// let num= 5.12113;
+// let num1= 0.72113;
+// console.log(Math.floor(5.56))
+// console.log(Math.floor(5.12))
+// console.log(Math.ceil(5.12))
+// console.log(Math.ceil(5.15))
+// console.log(Math.round(5.45))
+// console.log(Math.round(5.56))
+// console.log(Math.trunc(num))
+// console.log(Math.trunc(-5.8))
+// console.log(Math.floor(-5.8))
+// console.log(Math.ceil(-5.1))
+
+// console.log(Math.trunc(Math.random()*100000))
+// console.log(
+//   Math.min(1, 3),
+//   Math.max(45,124),
+//   Math.pow(1, 5),
+//   Math.sqrt(25),
+//   Math.cbrt(27)
+// ); //kub ildiz ostida
+// console.log(
+// Math.sign(1),
+// Math.sign(0),
+// Math.sign(-1),
+// Math.sign(-0),
+// Math.sign('12'),
+// Math.sign('-1'))
+/// ********************************vazifa***********************************************
+// let num = 143;
+// let a=0,b=0;
+
+// console.log(num.toString(2))
+// let sNum = +num.toString(2)
+// while(sNum){
+//   console.log(`lastNumber : ${sNum%10}`);
+//   if(sNum%10 ==1){
+//     a++;
+//   }else(b++)
+//   sNum = Math.trunc(sNum/10)
+// }
+// console.log( `${a}ta bir \n${b}ta nol ` )
+
+
+// while(num){
+//   console.log(`salom ${num}`);
+//   num = Math.trunc(num/10);
+// }
+//2. 
+// let min =10;
+// let max =20;
+
+// console.log(Math.trunc(Math.random()*max+min))
+
+
+// const randomInteger =(max,min)=>{
+// return Math.trunc(Math.random()*(max-min)+min)
+// }
+
+// console.log(randomInteger(20,10));
+
+let obj = {
+  name:'salom' ,child:null
+}
+ console.log(obj?.child?.age)// suroq xatolikni  oldini oladi
+
+
+
+
+
+
+
+
+
