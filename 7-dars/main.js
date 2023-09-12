63135d752009e9e381099ac7ada4cef1b99d082a
Jamshid <jsaminjonov5705@gmail.com>
2023-09-12 07:38:55 +0500

    object-2 advanced

--- 7-dars/main.js
+++ 7-dars/main.js
@@ -1,34 +1,35 @@
-let obj = {
-  age: 145,
-  name: "sunnat",
-  surName: "lazizov",
-  "phone number": 123456789,
-  online: true,
-  ofline: false,
+// let obj = {
+//   age: 145,
+//   name: "sunnat",
+//   surName: "lazizov",
+//   "phone number": 123456789,
+//   online: true,
+//   ofline: false,
 
-  child: {
-    age: 45,
-    surName: "lazizov",
-    "phone number": 123456789,
-    online: true,
+//   child: {
+//     age: 45,
+//     surName: "lazizov",
+//     "phone number": 123456789,
+//     online: true,
 
-    child: {
-      age: 25,
-      surName: "lazizov",
-      "phone number": 123456789,
-      online: true,
-      child: {
-        age: 25,
-        surName: "lazizov",
-        "phone number": 123456789,
-        online: true,
-      },
-    },
-  },
-};
-do{
-    if(obj.child){
-        console.log(obj.age)
-    }else break;
-    obj = obj.child;
-}while(1) 
+//     child: {
+//       age: 25,
+//       surName: "lazizov",
+//       "phone number": 123456789,
+//       online: true,
+//       child: {
+//         age: 25,
+//         surName: "lazizov",
+//         "phone number": 123456789,
+//         online: true,
+//       },
+//     },
+//   },
+// };
+// do{
+//     if(obj.child){
+//         console.log(obj.age)
+//     }else break;
+//     obj = obj.child;
+// }while(1) 
+// object advanced
