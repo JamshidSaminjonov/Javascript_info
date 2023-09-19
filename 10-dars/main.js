87ea4ea51b80b8e9cb4b04bf5853df2d10b3a884
Jamshid <jsaminjonov5705@gmail.com>
2023-09-20 01:16:56 +0500

    Array-1

---
+++ 10-dars/main.js
@@ -0,0 +1,23 @@
+let arr = [1, "webrain", function test() {}, null, undefined, true, false];
+// console.log(arr[0]);
+// console.log(arr[arr.length-1]);
+// console.log(arr.at(-1))
+let ar = new Array("5");
+let array = [];
+array[100] = 1;
+console.log(array);
+for (value of arr) {
+  console.log(value);
+}
+// ar[5] = 5;
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// document.getElementById("demo").innerHTML = fruits.toString();
+// console.log(arr.toString("."));
+// console.log(arr.join("."));
+// console.log(ar.length);
+// // console.log(ar.length);
+// console.log(Array.isArray(ar));
+// console.log([] == 0);
+// const person = ["John", "Doe", 46];
+// console.log(arr.sort());
+console.log(ar);
