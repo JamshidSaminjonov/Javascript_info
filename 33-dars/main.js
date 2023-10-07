2d1bfe255f8a0f6cbd92875fb402f5484393ee92
Jamshid <jsaminjonov5705@gmail.com>
2023-10-07 07:01:23 +0500

    DOM-1

---
+++ 33-dars/main.js
@@ -0,0 +1,46 @@
+/////// window/////////
+//----DOM=> document object model,,,CSSOM=> css object model
+//----BOM=>browser object model
+//----Javascript=> function,array,object
+
+// alert("salom");
+// window.alert("salom"); bir xil degani
+// alert(window.innerHeight)
+// alert(window.innerWidth) joriy height va joriy widthni beradi
+// alert(location.host);
+// alert(location.href);
+// console.log(alert(location.pathname));
+// console.log(document);
+// console.log(window);
+// let data = Document.getElementsByTagName("ul");
+// console.log(data);
+// let info = document.getElementsByTagName("h1");html collection holatda
+// let info = document.getElementsByTagName("h1")[0]; kerak malumotni chiqaradi
+// console.log(info);
+// let info = document.getElementsByClassName("parent");
+// var data = document.getElementsByTagName("h1");
+// let data = document.querySelector("ul"); togri kerak malumotni oladi
+// let data = document.querySelector("ul");
+// console.log(data.children); // ichidagi lilarni
+// console.log(data.childNodes); // space bilan lilarni alohida alohida child qilib oladii buni tree der ekan
+// console.log(data.parentNode);
+// console.log(data.parentElement);
+// console.log(data.firstChild); //-text-ni oladi // nodeslardan oladi
+// console.log(data.firstElementChild); //-li-ni oladi // elementsni oladi
+// console.log(data.lastElementChild.previousSibling.previousSibling);
+// console.log(data.lastElementChild.previousElementSibling); //elemnti bor holatda yozish eng yaxshisi
+// let by = document.getElementsByTagName("body"); // bu holatda doim  array qaytaradi  chunki istalgancha malumot buladi
+// let id = document.getElementById("set");
+// let qrId = document.querySelector("#set"); // query sellector faqat bittasini chiqaradi
+// // '.classname//#idname//
+// let qrId1 = document.querySelectorAll("#set");
+// console.log(id);
+// console.log(qrId1);
+// console.log(set.innerHTML);
+// // console.log(qrId.innerText);
+// // console.log(qrId.textContent);
+// set.innerHTML = "<h1>1</h1>";// agar id bitta bulsa uni nomini uzini ishlatb ketsa ham bulaveradi
+// // qrId.innerText = "<h1>1</h1>";
+// // qrId.textContent = "<h1>2</h1>";
+// qrId.hidden = true;
+// qrId.hidden = false;
