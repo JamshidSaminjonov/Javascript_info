6cc8d866772af06a4d3f7c7259b9e81f9cdaec31
Jamshid <jsaminjonov5705@gmail.com>
2023-10-09 16:48:48 +0500

    Dropdown

---
+++ homework/Dropdown/main.js
@@ -0,0 +1,8 @@
+let drops = document.querySelector(".drops");
+let triangle = document.querySelector("i");
+
+const drop = () => {
+  drops.classList.toggle("active");
+  triangle.classList.toggle("rotate");
+};
+console.log();
